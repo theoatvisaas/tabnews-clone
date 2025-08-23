b447d177b2ab8e97a5d4e9b7051bc42a1f78ddd0
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2025-08-23 15:56:20 -0300

    finish the /status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,31 @@
 import database from "infra/database.js";
 
 const status = async (req, res) => {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  res.status(200).json({ status_message: "All systems operational" });
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenConnectionsResult = await database.query({
+    text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const dbOpenConnectionsValue = dbOpenConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: parseInt(dbOpenConnectionsValue),
+      },
+    },
+  });
 };
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+
+  const resBody = await res.json();
+  const parsedUpdatedAt = new Date(resBody.updated_at).toISOString();
+
+  expect(resBody.updated_at).toBeDefined();
+  expect(parsedUpdatedAt).toEqual(resBody.updated_at);
+
+  expect(resBody.dependencies.database.version).toBe("16.3");
+  expect(resBody.dependencies.database.max_connections).toEqual(100);
+  expect(resBody.dependencies.database.opened_connections).toEqual(1);
 });
