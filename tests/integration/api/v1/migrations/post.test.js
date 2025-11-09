30fd1b47034e78fef92a1f17d0bc5aba7422ca99
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2025-11-09 18:16:02 -0300

    adds api/v1/migrations endpoint

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,29 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(res1.status).toBe(201);
+
+  const response1Body = await res1.json();
+
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
+
+  const res2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(res2.status).toBe(200);
+
+  const response2Body = await res2.json();
+
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
+});
